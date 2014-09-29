47323d51c3bc01aaf0cd1d3ba7ed506730144d7d
Nicholas Wiersma <nick@wiersma.co.za>
2014-09-29 21:10:23 +0200

    Switch head for kopf

--- configure/recipes/esplugins.rb
+++ configure/recipes/esplugins.rb
@@ -5,11 +5,11 @@ execute "install_plugin_es_aws" do
     not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/cloud-aws") }
 end
 
-execute "install_plugin_es_head" do
+execute "install_plugin_es_kopf" do
     user "root"
     cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
-    command "plugin -install mobz/elasticsearch-head"
-    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
+    command "plugin -install lmenezes/elasticsearch-kopf"
+    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/kopf") }
 end
 
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
