d96d3cd3779af02c9b49cf553d9c8370410364d7
Nicholas Wiersma <nick@wiersma.co.za>
2014-09-29 16:51:10 +0200

    Install was plugin myself

--- configure/recipes/esplugins.rb
+++ configure/recipes/esplugins.rb
@@ -1,3 +1,13 @@
+script "install_plugin_es_aws" do
+	interpreter "bash"
+	user "root"
+	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+	code <<-EOH
+  	plugin -install elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}
+  	EOH
+	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/cloud-aws") }
+end
+
 script "install_plugin_es_head" do
 	interpreter "bash"
 	user "root"
