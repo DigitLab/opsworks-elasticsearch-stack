f0e889c9e68331cb017995e3caa5ebe3e0117bc8
Nicholas Wiersma <nick@wiersma.co.za>
2014-09-29 18:29:22 +0200

    Switch out script for execute, as script seems to be failing on AWS

--- configure/recipes/esplugins.rb
+++ configure/recipes/esplugins.rb
@@ -1,17 +1,15 @@
-script "install_plugin_es_aws" do
-	interpreter "bash"
-	user "root"
-	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
-	code "plugin -install elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
-	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/cloud-aws") }
+execute "install_plugin_es_aws" do
+    user "root"
+    cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+    command "plugin -install elasticsearch/elasticsearch-cloud-aws/#{node.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version']}"
+    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/cloud-aws") }
 end
 
-script "install_plugin_es_head" do
-	interpreter "bash"
-	user "root"
-	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
-	code "plugin -install mobz/elasticsearch-head"
-	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
+execute "install_plugin_es_head" do
+    user "root"
+    cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
+    command "plugin -install mobz/elasticsearch-head"
+    not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
 
 #notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
