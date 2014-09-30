129e903d84686f04601e8a88c94d39bad1ea07d9
Nicholas Wiersma <nick@wiersma.co.za>
2014-09-30 10:46:54 +0200

    Reconfigure to install stuff properly

--- configure/recipes/monit.rb
+++ configure/recipes/monit.rb
@@ -1,5 +1,5 @@
 template File.join(node[:monit][:conf_dir], "elasticsearch.monitrc") do
-  source "elasticsearch.monitrc.conf.erb"
+  source "elasticsearch.monitrc.erb"
   mode 0440
   owner "root"
   group "root"
