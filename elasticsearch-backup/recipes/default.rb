fe2c5720afa2080cf4c63f045cbe9ff14fc5faea
Nicholas Wiersma <nick@wiersma.co.za>
2015-01-15 07:59:07 +0200

    Changed backup method to use cron.daily folder rather

--- elasticsearch-backup/recipes/default.rb
+++ elasticsearch-backup/recipes/default.rb
@@ -1,10 +1,8 @@
-include_recipe "elasticsearch"
-
 package "jq"
 
-cron "elasticsearchbackup" do
-    minute "0"
-    hour "0"
-    command "curl -s -S -XGET \"#{node["elasticsearch-backup"][:host]}/_snapshot/#{node["elasticsearch-backup"][:repository]}/_all?pretty=true\" | jq '.snapshots[] | .snapshot + \" \" + .end_time' | sed 's/^.\(.*\).$/\1/' | sort -k 2 -r | awk '{ if (NR > 30) { system(\"curl -XDELETE \" \"#{node["elasticsearch-backup"][:host]}/_snapshot/#{node["elasticsearch-backup"][:repository]}/\"$1) } } END { system(\"curl -XPUT \" \"#{node["elasticsearch-backup"][:host]}/_snapshot/#{node["elasticsearch-backup"][:repository]}/`date +\%s`\") }'"
-    action :create
+template "/usr/cron.daily/elasticsearch-backup" do
+  source "elasticsearch-backup.erb"
+  mode "0755"
+  owner "root"
+  group "root"
 end
